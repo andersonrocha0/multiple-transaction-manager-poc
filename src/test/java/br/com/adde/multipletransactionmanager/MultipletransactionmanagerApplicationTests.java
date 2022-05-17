5fc405b58dfd9d8f7536fd540b97021789979d54
Anderson Rocha <anderson.rocha@kwan.pt>
2022-05-17 13:17:08 +0100

    First commit

---
+++ src/test/java/br/com/adde/multipletransactionmanager/MultipletransactionmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adde.multipletransactionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipletransactionmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
